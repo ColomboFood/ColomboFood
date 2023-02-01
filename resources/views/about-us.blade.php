fe7ecc23645b204a888ba903da7c2a394d70e633
xTalAlex <talale93@hotmail.it>
2023-02-01 17:58:00 +0100

    About Us update

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 
-    <div class="w-full bg-bottom bg-cover h-64 lg:h-[32rem] " style="background-image: url('/img/homebanner.png')">
+    {{-- <div class="w-full bg-bottom bg-cover h-64 lg:h-[32rem] " style="background-image: url('/img/bambino.png')">
         <div class="py-12 mx-auto lg:container lg:pt-32">
             <div class="w-full px-6 py-4 mx-auto text-center bg-white lg:text-left lg:px-2 lg:w-96 lg:p-auto bg-opacity-80 lg:bg-opacity-0 lg:ml-20">
                 <div class="mx-auto max-w-max">
@@ -8,26 +8,28 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
+    <div class="w-full bg-bottom bg-cover h-64 lg:h-[32rem] " style="background-image: url('/img/bambino.png')"></div>
 
     <div class="container max-w-5xl px-6 py-12 mx-auto mb-12 prose">
         <div>
-            <h2 class="mb-12 text-2xl font-bold">{{ __('About Us') }}</h2>
+            <h2 class="mb-12 text-2xl font-bold">{{ __('Colombo Food') }}</h2>
             <p>
-                Ricordi spensierati trascorsi insieme a mio papà, tra bancali, furgoni e soprattutto tanti dolci e cibo
-                di qualità. È da qui che nasce il mio interesse e la mia passione per il buon cibo e la sua distribuzione.
+                Ricordi spensierati trascorsi insieme a mio padre imparando a riconoscere il cibo di qualità,
+                e poi via tra bancali, furgoni e soprattutto tanti clienti che per mio padre erano amici.
             </p>
             <p>
-                È da qui che nel 2016 si realizza il mio sogno di bambino. Nasce Colombo Food.
+                Sono cresciuto in questo mondo e nel 2016 fu naturale seguire la mia aspirazione.<br>
+                Nacque Colombo Food.
             </p>
             <p>
-                Colombo Food è oramai un'azienda consolidata nel settore e in continua crescita grazie alla passione
-                e ai valori che portiamo avanti tutti insieme. È un lavoro difficile, orari massacranti, dedizione
-                completa al cliente. Ma la vostra soddisfazione è il nostro motivo d'orgoglio.
+                Colombo Food è un'azienda consolidata nel settore della ricerca e distribuzione di prodotti di qualità.
+                Ma la vera forza che ci permette dopo tanti anni di continuare a crescere, è la capacità di ascoltare e
+                capire. Abbiamo tanti clienti ma per noi sono amici.
             </p>
             <p>
-                Il nostro team ha poche regole ma precise; <strong>il prodotto deve essere di qualità eccellente, la consegna
-                al cliente deve essere puntuale e discreta</strong> e non bisogna mai smettere di cercare di migliorarsi.
+                Abbiamo poche regole ma chiare; <strong>il prodotto deve essere di qualità eccellente e la consegna al
+                cliente deve essere puntuale e discreta</strong>.
             </p>
             <p>
                 E oggi facciamo un altro passo per la nostra crescita con la creazione di colombofood.it; una piattaforma
